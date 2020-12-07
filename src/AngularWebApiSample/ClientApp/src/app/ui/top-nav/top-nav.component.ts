318472101c4b0f5c1aa85c73c2a4235edab38e3b
Adam Pluciński <adaskothebeast@gmail.com>
2020-12-07 18:26:04 +0100

    upgraded libs

--- src/AngularWebApiSample/ClientApp/src/app/ui/top-nav/top-nav.component.ts
+++ src/AngularWebApiSample/ClientApp/src/app/ui/top-nav/top-nav.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.scss'],
 })
 export class TopNavComponent {
   public show = false;
